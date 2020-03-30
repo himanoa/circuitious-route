5cc48aa3762333046d601cb529f52190dcc5a5df
himanoa <matsunoappy@gmail.com>
2020-03-31 03:56:53 +0900

    Exec `yarn add sql-template-strings`

--- simic/backend/src/index.ts
+++ simic/backend/src/index.ts
@@ -1,17 +1,25 @@
 import * as Express from "express";
 import { validate, defineSchema } from "@japan-d2/schema"
+import * as sqlite from "sqlite"
 import { promisify } from "util"
 
 if(typeof process.env.DATABASE_PATH !== "string") {
   throw Error("Enviroment variable not found: DATABASE_PATH")
 }
-const db = new sqlite3.Database(process.env.DATABASE_PATH)
+const db = sqlite.open(process.env.DATABASE_PATH, { promise: Promise })
 
 const app = Express.default()
 
-app.post("/", (req, res) => {
+function runAsyncWrapper (callback: (req: Express.Request, res: Express.Response, next?: Express.NextFunction) => Promise<void>) {
+  return function (req: Express.Request, res: Express.Response, next: Express.NextFunction) {
+    callback(req, res, next)
+      .catch(next)
+  }
+}
+
+app.post("/", runAsyncWrapper(async (req, res) => {
   const inputSchema = defineSchema().string("discordId")
   if(validate(req.body, inputSchema)) {
     req.body.discordId
   }
-})
+}))
