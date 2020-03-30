91b0d0dc998772885f59616a9f7f756188201458
himanoa <matsunoappy@gmail.com>
2020-03-31 07:30:04 +0900

    WIP

---
+++ simic/backend/src/handlers/login-url-handler.ts
@@ -0,0 +1,30 @@
+import { defineSchema, ValidationError, assertValid } from "@japan-d2/schema"
+import { SQL, SQLStatement } from "sql-template-strings"
+import * as Express from "express";
+
+export const issueLoginUrlHandler:(deps: {
+  executeQuery: Promise<(sql: string | SQLStatement) => Promise<any>>,
+  generateRandomString: () => void
+}) => (req: Express.Request, res: Express.Response) => Promise<void> = (deps) => async (req, res) => {
+  const executeQuery = await deps.executeQuery
+  const inputSchema = defineSchema().number("discordId")
+  try {
+    assertValid(req.body, inputSchema)
+    const loginId = deps.generateRandomString()
+      if ((await executeQuery(SQL`SELECT * FROM USERS WHERE discord_id = ${req.body.discordId}`)).length === 0) {
+        await executeQuery(SQL`INSERT INTO users (discord_id, current_login_id), VALUES (${req.body.discordId}, ${loginId})`)
+      } else {
+        await executeQuery(SQL`UPDATE users SET loginId = ${loginId} WHERE discord_id = ${req.body.discordId}`)
+      }
+
+      res.status(201).json({ loginId })
+  } catch (err) {
+    if(err instanceof ValidationError) {
+      res.status(400).json({ error: err })
+    } else {
+      res.status(500).json({
+        error: err
+      })
+    }
+  }
+}
