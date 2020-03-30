db01dd20c60afc286dff55e9f803511bdaa39755
himanoa <matsunoappy@gmail.com>
2020-03-31 05:01:15 +0900

    Implement issue login url

--- simic/backend/src/index.ts
+++ simic/backend/src/index.ts
@@ -1,7 +1,9 @@
 import * as Express from "express";
-import { validate, defineSchema } from "@japan-d2/schema"
+import { validate, defineSchema, ValidationError } from "@japan-d2/schema"
 import * as sqlite from "sqlite"
-import { promisify } from "util"
+import { SQL, SQLStatement } from "sql-template-strings"
+import { randomBytes }  from "crypto"
+import { format } from "url"
 
 if(typeof process.env.DATABASE_PATH !== "string") {
   throw Error("Enviroment variable not found: DATABASE_PATH")
@@ -17,9 +19,35 @@ function runAsyncWrapper (callback: (req: Express.Request, res: Express.Response
   }
 }
 
-app.post("/", runAsyncWrapper(async (req, res) => {
+export const generateUrlHandler:(deps: {
+  executeQuery: Promise<(sql: string | SQLStatement) => Promise<any>>
+}) => (req: Express.Request, res: Express.Response) => Promise<void> = (deps) => async (req, res) => {
+  const executeQuery = await deps.executeQuery
   const inputSchema = defineSchema().string("discordId")
   if(validate(req.body, inputSchema)) {
-    req.body.discordId
+    const loginId = randomBytes(16).toString("hex")
+    try {
+      if ((await executeQuery(SQL`SELECT * FROM USERS WHERE discord_id = ${req.body.discordId}`)).length === 0) {
+        await executeQuery(SQL`INSERT INTO users (discord_id, current_login_id), VALUES (${req.body.discordId}, ${loginId})`)
+      } else {
+        await executeQuery(SQL`UPDATE users SET loginId = ${loginId} WHERE discord_id = ${req.body.discordId}`)
+      }
+
+      res.status(201).json({ loginUrl: format({ protocol: req.protocol, host: req.get("host"), pathname: `/login/${loginId}` }) })
+    } catch (err) {
+      if(err instanceof ValidationError) {
+        res.status(400).json({ error: err })
+      } else {
+        res.status(500).json({
+          error: err
+        })
+      }
+    }
   }
-}))
+}
+
+app.post("/", runAsyncWrapper(generateUrlHandler(
+  {
+    executeQuery: db.then(db => new Promise((resolve) => resolve(db.get)))
+  }
+)))
