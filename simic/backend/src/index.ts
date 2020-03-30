c0944d058521ad2a45b7290cc532216fff830138
himanoa <matsunoappy@gmail.com>
2020-03-31 01:57:59 +0900

    WIP

--- simic/backend/src/index.ts
+++ simic/backend/src/index.ts
@@ -1,6 +1,11 @@
 import * as Express from "express";
+import { validate, defineSchema } from "@japan-d2/schema"
 
 const app = Express.default()
 
 app.post("/", (req, res) => {
+  const inputSchema = defineSchema().string("discordId")
+  if(validate(req.body, inputSchema)) {
+    req.body.discordId
+  }
 })
