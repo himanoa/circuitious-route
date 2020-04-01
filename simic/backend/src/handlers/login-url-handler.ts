e7d18098f63015db86735b690ec1c676e90913a0
himanoa <matsunoappy@gmail.com>
2020-04-02 01:59:11 +0900

    WIP

--- simic/backend/src/handlers/login-url-handler.ts
+++ simic/backend/src/handlers/login-url-handler.ts
@@ -1,26 +1,42 @@
 import { defineSchema, ValidationError, assertValid } from "@japan-d2/schema"
 import { SQL, SQLStatement } from "sql-template-strings"
 import * as Express from "express";
+import { UserNotFoundError } from "../error"
+
+type Token = string
 
 export const issueAuthorizedTokenHandler:(deps: {
   executeQuery: Promise<(sql: string | SQLStatement) => Promise<any>>,
-  generateRandomString: () => void
+  sign: (obj: { discordId: string }) => Token,
+  generateRandomString: () => string
 }) => (req: Express.Request, res: Express.Response) => Promise<void> = (deps) => async (req, res) => {
   const executeQuery = await deps.executeQuery
+  const inputSchema = defineSchema().number("discordId")
+  const payload = req.body
   try {
-    assertValid(req.body, inputSchema)
-    const loginId = deps.generateRandomString()
-      if ((await executeQuery(SQL`SELECT * FROM USERS WHERE discord_id = ${req.body.discordId}`)).length === 0) {
-        await executeQuery(SQL`INSERT INTO users (discord_id, current_login_id), VALUES (${req.body.discordId}, ${loginId})`)
-      } else {
-        await executeQuery(SQL`UPDATE users SET loginId = ${loginId} WHERE discord_id = ${req.body.discordId}`)
-      }
+    assertValid(payload, inputSchema)
+    const refreshToken = await executeQuery(SQL`SELECT * FROM refresh_tokens WHERE discord_id = ${payload.discordId} AND enabled = 1;`)
+    const user = await executeQuery(SQL`SELECT * FROM users WHERE discord_id = ${payload.discordId};`)
 
-      res.status(201).json({ loginId })
+    if(!user) {
+      throw new UserNotFoundError(`${payload.discordId} is not found`)
+    }
+    if(refreshToken.length === 0) {
+      const refreshToken = deps.generateRandomString()
+      await executeQuery(SQL`INSERT INTO refresh_tokens (discord_id, token, activated, created_at) VALUES (${payload.discordId}, ${refreshToken}, 1, strftime('%s', 'now'));`)
+      res.status(201).json({
+        refreshToken: refreshToken,
+        accessToken: deps.sign({discordId: payload.discordId.toString()})
+      })
+    } else {
+      res.status(401).json({})
+    }
   } catch (err) {
     if(err instanceof ValidationError) {
       res.status(400).json({ error: err })
-    } else {
+    } else if(err instanceof UserNotFoundError) {
+      res.status(401).json({ error: err })
+    }  else {
       res.status(500).json({
         error: err
       })
