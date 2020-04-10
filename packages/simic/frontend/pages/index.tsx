98ec52eaa5226fe20c09ffcd1959df815fcc412e
himanoa <matsunoappy@gmail.com>
2020-04-11 06:18:28 +0900

    Ignore yarn-error.log

--- packages/simic/frontend/pages/index.tsx
+++ packages/simic/frontend/pages/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useCallback } from "react";
 import {
   Button,
   ButtonGroup,
@@ -6,12 +6,17 @@ import {
   Paper,
   FormControl,
   TextField,
+  Typography,
   makeStyles,
 } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
+  },
+  commentPaper: {
+    padding: theme.spacing(2),
+    color: theme.palette.text.secondary,
   },
   paper: {
     padding: theme.spacing(2),
@@ -21,6 +26,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 const Index: React.FC = () => {
   const classes = useStyles();
+  const [streamKey, setStreamKey] = useState("")
+  const handleStreamKeyInputChange = useCallback((e: React.ChangeEvent<HTMLInputElement>) => setStreamKey(e.target.value), [])
+
   return (
     <div className={classes.root}>
       <Grid
@@ -29,26 +37,41 @@ const Index: React.FC = () => {
         alignContent="center"
         alignItems="center"
         direction="column"
+        spacing={10}
       >
-        <Paper className={classes.paper} >
-          <Grid container spacing={10} justify="center" direction="column">
-            <Grid item direction="column">
-              <FormControl>
-                <TextField label="ストリームキー"></TextField>
-              </FormControl>
+        <Grid item>
+          <Paper className={classes.paper} >
+            <Grid container spacing={10} justify="center" direction="column" alignItems="center">
+              <Grid item direction="column">
+                <FormControl>
+                  <TextField label="ストリームキー" onChange={handleStreamKeyInputChange} value={streamKey}></TextField>
+                </FormControl>
+              </Grid>
+              <Grid item>
+                <ButtonGroup>
+                  <Button color="primary" variant="contained">
+                    ストリームキーの設定の保存
+                  </Button>
+                  <Button color="secondary" variant="contained">
+                    配信の開始
+                  </Button>
+                </ButtonGroup>
+              </Grid>
             </Grid>
-            <Grid item>
-              <ButtonGroup>
-                <Button color="primary" variant="contained">
-                  ストリームキーの設定の保存
-                </Button>
-                <Button color="secondary" variant="contained">
-                  配信の開始
-                </Button>
-              </ButtonGroup>
+          </Paper>
+        </Grid>
+        <Grid item spacing={10}>
+          <Paper className={classes.commentPaper}>
+            <Grid container spacing={5} direction="column">
+              <Grid item>
+                <Typography variant="subtitle1" component="h2">コメント</Typography>
+              </Grid>
+              <Grid item>
+                <Typography>配信が開始されたらここにコメントが流れます。</Typography>
+              </Grid>
             </Grid>
-          </Grid>
-        </Paper>
+          </Paper>
+        </Grid>
       </Grid>
     </div>
   );
