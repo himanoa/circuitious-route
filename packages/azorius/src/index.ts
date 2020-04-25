345f386d883c1e34789994d8d077a30e2b2b731c
himanoa <matsunoappy@gmail.com>
2020-04-25 16:14:16 +0900

    Setup redis pub/sub

--- packages/azorius/src/index.ts
+++ packages/azorius/src/index.ts
@@ -1,6 +1,7 @@
 import { Client } from "discord.js";
 import { getLogger, configure } from "log4js";
 import { routes } from "./default-routes";
+import redis from "redis";
 
 configure({
   appenders: { out: { type: "stdout" }, err: { type: "stderr" } },
@@ -18,6 +19,25 @@ configure({
 const logger = getLogger();
 const errorLogger = getLogger("error");
 const client = new Client();
+
+if (typeof process.env.REDIS_URL !== "string") {
+  throw Error("Enviroment variable not found: REDIS_URL");
+}
+
+const subscriber = redis.createClient(process.env.REDIS_URL);
+const publisher = redis.createClient(process.env.REDIS_URL);
+subscriber.on("error", (error) => console.error(error));
+publisher.on("error", (error) => console.error(error));
+
+subscriber.on("message", (channel, value) => {
+  logger.info(`${channel}: ${value}`);
+});
+
+client.on("ready", () => {
+  client.users.cache.forEach((u) => {
+    subscriber.subscribe(u.id);
+  });
+});
 
 client.on("message", (message) => {
   if (!message.author.bot) {
