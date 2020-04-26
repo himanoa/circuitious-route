252399cf0e874bc253b483bd2a90c623b16f4e39
himanoa <matsunoappy@gmail.com>
2020-04-26 14:12:00 +0900

    feat(azorius): Join the voice channel when you receive the start streaming event

---
+++ packages/azorius/src/handlers/pubsub/index.ts
@@ -0,0 +1,15 @@
+import { Events } from "izett"
+import { startStreamingHandler } from "./start-streaming-handler"
+import { Client } from "discord.js"
+import { Logger } from "log4js"
+
+type Dependencies = {
+  discordClient: Client,
+  logger: Logger
+}
+
+export const handler = (deps: Dependencies) => (event: any) => {
+  if(event.type === "start-streaming") {
+    startStreamingHandler(deps)(event)
+  }
+}
